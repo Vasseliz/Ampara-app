58e508fd0dd15880b4e015810d29a3d09b080a55
Vasseliz <nicolaszielasko@gmail.com>
2026-04-02 23:47:18 -0300

    Adicionando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import "./App.css";
-import Cofre from "./features/Cofre";
+import Cofre from "./features/cofre/Cofre";
+import { Prontuario } from "./features/prontuario/Prontuario";
+import Home from "./pages/Home";
+import { PacienteRoute } from "./routes/PacienteRoute";
+import { ProfissionalRoute } from "./routes/ProfissionalRoute";
 
 function App() {
-  return (<Cofre />);
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route element={<PacienteRoute />}>
+          <Route path="/cofre" element={<Cofre />} />
+        </Route>
+
+        <Route path="/profissional" element={<ProfissionalRoute />}>
+          <Route path="prontuario" element={<Prontuario />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
